8b33495cf92fac7fe285381186ad2ea3c3d357f8
GMkonan <guilhemont@hotmail.com>
2025-01-24 20:06:02 -0300

    Add initial issue cmd and root

---
+++ cmd/issue.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var message string
+
+// issueCmd represents the issue command
+var issueCmd = &cobra.Command{
+	Use:   "issue",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("issue called")
+
+		fmt.Println(message)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(issueCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// issueCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	issueCmd.Flags().StringVarP(&message, "message", "m", "", "Title of the issue")
+}
--- cmd/root.go
+++ cmd/root.go
@@ -1,23 +1,17 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
-	"os"
-
 	"github.com/spf13/cobra"
+	"os"
 )
 
-
-
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "linear-cli",
-	Short: "A brief description of your application",
+	Use:   "lc",
+	Short: "a CLI for linear app",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
+
+	lc issue -m "this is an awesome issue title"
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
@@ -27,8 +21,6 @@ to quickly create a Cobra application.`,
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -47,5 +39,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
